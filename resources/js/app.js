d4194e1545da3b7d79fd16116b9e4d976b1fe1af
taanafana16 <taanafanurulainun16@gmail.com>
2024-07-19 15:46:10 +0700

    Install Swiper

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,30 @@
 import './bootstrap';
+
+// import Alpine JS
 import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
 
 Alpine.start()
+
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
