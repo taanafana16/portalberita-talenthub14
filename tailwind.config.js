ddf0319ca154f3e706c8a41ba4ef360bc2e175e2
taanafana16 <taanafanurulainun16@gmail.com>
2024-07-22 17:23:10 +0700

    Konfigurasi Package

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'custom-light': '#E1ECC8',
+      'custom-light-green' : '#AFC8AD',
+      'custom-green' : '#88AB8E',
+      'custom-dark-green' : '#4F6F52',
+      'custom-dark-dark-green' : '#1C6758',
+      'custom-footer' : '#83B582',
+      'custom-color-text-table' : '#4F6F52',
+    },},
   },
   plugins: [],
 }
