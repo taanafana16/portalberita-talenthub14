cf4c4e758a91546a3852bf3b046516dc0e87ecaa
taanafana16 <taanafanurulainun16@gmail.com>
2024-07-22 17:30:47 +0700

    Konfigurasi routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('landingpage');
+//Admin
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('dashboard');
+    Route::resource('/berita', BeritaController::class);
+    Route::resource('/kategori', KategoriController::class);
+    Route::get('/pengaturan', [AdminController::class, 'pengaturan'])->name('pengaturan');
 });
+//User
+Route::get('/', function () {
+    return view('/user/landingpage');
+});
+Route::get('/politik', function () {
+    return view('/user/politik');
+})->name('politik');
+Route::get('/ekonomi', function () {
+    return view('/user/ekonomi');
+})->name('ekonomi');
+Route::get('/teknologi', function () {
+    return view('/user/teknologi');
+})->name('teknologi');
+Route::get('/hiburan', function () {
+    return view('/user/hiburan');
+})->name('hiburan');
+Route::get('/olahraga', function () {
+    return view('/user/olahraga');
+})->name('olahraga');
